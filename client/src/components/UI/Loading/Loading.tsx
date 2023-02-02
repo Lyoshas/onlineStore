de96b88bd007e8ec5ce9f055651224697e833e2d
Oleksii Potapchuk <lyoshachuk@gmail.com>
2023-02-02 15:12:58 +0200

    feat: modify the <Loading /> component to change its color
    Previously, the Loading component could only be white.
    Now this component accepts the "color" prop that specifies what color
    this loading spinner should have. This prop should be a string. You can
    specify any color, like "white", "rgb(255, 255, 0)", "#f0f", etc.
    If the "color" prop is not specified, the default "#fff" is assumed.

--- client/src/components/UI/Loading/Loading.tsx
+++ client/src/components/UI/Loading/Loading.tsx
@@ -2,11 +2,19 @@ import { FC } from 'react';
 
 import classes from './Loading.module.css';
 
-const Loading: FC<{ width?: string; height?: string }> = (props) => {
+const Loading: FC<{ width?: string; height?: string; color?: string }> = (
+    props
+) => {
     return (
         <span
             className={classes.loader}
-            style={{ width: props.width, height: props.height }}
+            style={{
+                width: props.width,
+                height: props.height,
+                borderColor: props.color || '#fff',
+                // borderBottomColor was added here for technical reasons. If this remained in the CSS file, borderColor would overwrite this property
+                borderBottomColor: 'transparent',
+            }}
         ></span>
     );
 };
