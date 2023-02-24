745c604ec33fa4abda81ffce30afdd332a99d8e4
Oleksii Potapchuk <lyoshachuk@gmail.com>
2023-02-24 12:17:33 +0200

    refactor: use 'react-google-recaptcha' for captcha logic
    This commit changes the way ReCAPTCHA is handled. Instead of writing
    the <ReCAPTCHA /> component from scratch, now this custom component
    uses the ReCAPTCHA component from the 'react-google-recaptcha' package.
    Previously, to connect the captcha it was necessary to include a script
    in index.html (not in the component because otherwise it wouldn't work).
    This new change makes the recaptcha more modular.
    
    This change would prevent a bug where the captcha isn't loaded when
    trying to create protected routes.

--- client/src/components/UI/reCAPTCHA/ReCAPTCHA.tsx
+++ client/src/components/UI/reCAPTCHA/ReCAPTCHA.tsx
@@ -1,7 +1,8 @@
-import { FC, Fragment } from 'react';
+import { FC } from 'react';
+import { useField } from 'formik';
+import NpmRecaptcha from 'react-google-recaptcha';
 
 import classes from './reCAPTCHA.module.css';
-import { useField } from 'formik';
 
 type OnCaptchaVerify = (recaptchaToken: string) => void;
 
@@ -16,28 +17,26 @@ const ReCAPTCHA: FC = () => {
     const [field, meta, helpers] = useField('recaptchaToken');
 
     // Google reCAPTCHA API expects string the defines the name of the function in the global namespace that should be fired, so it's not possible to pass a callback to data-callback attribute
-    window.onCaptchaVerify = (recaptchaToken: string) => {
+    const onCaptchaVerify = (recaptchaToken: string | null) => {
         console.log('recaptcha submission!');
         console.log(recaptchaToken);
         helpers.setValue(recaptchaToken);
         helpers.setError(undefined);
     };
 
-    window.onCaptchaExpired = () => {
+    const onCaptchaExpired = () => {
         console.log('captcha has expired');
         helpers.setValue('');
         helpers.setError('Captcha has expired. Please try again.');
     };
 
     return (
-        <Fragment>
-            <div
-                className={`g-recaptcha ${classes.recaptcha}`}
-                data-sitekey="6LciTjkkAAAAAFMcYF8Tu68clGCWGKrEy83GrsPz"
-                data-callback="onCaptchaVerify"
-                data-expired-callback="onCaptchaExpired"
-            ></div>
-        </Fragment>
+        <NpmRecaptcha
+            sitekey="6LciTjkkAAAAAFMcYF8Tu68clGCWGKrEy83GrsPz"
+            onChange={onCaptchaVerify}
+            onExpired={onCaptchaExpired}
+            className={classes.recaptcha}
+        />
     );
 };
 
