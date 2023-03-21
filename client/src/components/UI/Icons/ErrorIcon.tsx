ce49a25ba542fafcb784b8489c44de84ae16afd7
Oleksii Potapchuk <lyoshachuk@gmail.com>
2023-03-21 10:33:05 +0200

    fix: change image location for <ErrorIcon />

--- client/src/components/UI/Icons/ErrorIcon.tsx
+++ client/src/components/UI/Icons/ErrorIcon.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const ErrorIcon: FC<{ className?: string }> = (props) => {
     return (
         <img
-            src="/error-icon.svg"
+            src="/error-icon.png"
             className={props.className || ''}
             alt="Error icon"
         />
