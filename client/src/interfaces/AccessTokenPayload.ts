88b0b4fc0aaa2d99296a8da69f08a9784d0fbd65
Oleksii Potapchuk <lyoshachuk@gmail.com>
2023-06-01 14:33:10 +0300

    feat: add isAdmin status to the Redux store
    This commit adds a new property to the 'auth' slice called 'isAdmin'.
    It designates whether a user is an admin or not. This status is
    retrieved by decoding the JWT accessToken. This 'isAdmin' property will
    be used in the future to prevent users from accessing protected routes.
    
    There is no need to worry about verifying this token, because we got it
    from the server, so we can safely assume that it is valid. Even if for
    some reason the token is malformed, the user won't be able to perform
    any serious action, because the API routes are protected.

---
+++ client/src/interfaces/AccessTokenPayload.ts
@@ -0,0 +1,8 @@
+interface AccessTokenPayload {
+    id: number;
+    isAdmin: boolean;
+    iat: number;
+    exp: number;
+}
+
+export default AccessTokenPayload;
--- client/src/store/slices/auth.ts
+++ client/src/store/slices/auth.ts
@@ -1,12 +1,20 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import jwt_decode from 'jwt-decode';
+
+import AccessTokenPayload from '../../interfaces/AccessTokenPayload';
 
 // null - not determined yet
 interface AuthState {
     accessToken: string | null;
     isAuthenticated: boolean | null;
+    isAdmin: boolean | null;
 }
 
-const initialState: AuthState = { accessToken: null, isAuthenticated: null };
+const initialState: AuthState = {
+    accessToken: null,
+    isAuthenticated: null,
+    isAdmin: null,
+};
 
 const authSlice = createSlice({
     name: 'auth',
@@ -15,10 +23,17 @@ const authSlice = createSlice({
         updateAccessToken(state, action: PayloadAction<string>) {
             state.accessToken = action.payload;
             state.isAuthenticated = true;
+            
+            // if we have the access token, we can start decrypting it
+            // since we got this token from the API server, we can trust the data that's in it
+            // moreover, the server won't allow any unauthorized operations
+            const encoded: AccessTokenPayload = jwt_decode(action.payload);
+            state.isAdmin = encoded.isAdmin;
         },
         invalidateUser(state) {
             state.accessToken = '';
             state.isAuthenticated = false;
+            state.isAdmin = false;
         },
     },
 });
