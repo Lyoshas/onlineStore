a563dccb18ddcf5f5f4c5ce0e56fe3054b665d0e
Oleksii Potapchuk <lyoshachuk@gmail.com>
2023-06-13 11:43:58 +0300

    refactor(<ProductList />): improve type safety by handling null elements
    This commit enhances the type safety of the ProductList component and
    addresses a TypeScript suggestion. The code now includes a guard to
    check for null elements in the props.products array. If a null element
    is encountered, it is skipped, ensuring it won't be rendered on the
    screen. This approach replaces the usage of the non-null assertion
    operator '!' and improves type safety by handling potential null
    values explicitly.

--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -9,19 +9,23 @@ const ProductList: FC<{ products: (Omit<Product, 'category'> | null)[] }> = (
 ) => {
     return (
         <section className={classes['product-list']}>
-            {props.products.map((product) => (
-                <ProductItem
-                    key={product!.id}
-                    id={product!.id}
-                    title={product!.title}
-                    initialImageURL={product!.initialImageUrl}
-                    additionalImageURL={product!.additionalImageUrl}
-                    price={product!.price}
-                    shortDescription={product!.shortDescription}
-                    isAvailable={product!.isAvailable}
-                    isRunningOut={product!.isRunningOut}
-                />
-            ))}
+            {props.products.map((product) => {
+                if (product === null) return false;
+
+                return (
+                    <ProductItem
+                        key={product.id}
+                        id={product.id}
+                        title={product.title}
+                        initialImageURL={product.initialImageUrl}
+                        additionalImageURL={product.additionalImageUrl}
+                        price={product.price}
+                        shortDescription={product.shortDescription}
+                        isAvailable={product.isAvailable}
+                        isRunningOut={product.isRunningOut}
+                    />
+                );
+            })}
         </section>
     );
 };
