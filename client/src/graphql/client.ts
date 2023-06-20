dc3f596c8c569384a1762d20b7518d6e21606433
Oleksii Potapchuk <lyoshachuk@gmail.com>
2023-06-20 08:40:47 +0300

    feat: implement seamless access token renewal in Apollo Client
    This commit introduces a seamless access token renewal mechanism in
    Apollo Client for authentication. The setContext function from
    @apollo/client/link/context is used to intercept outgoing requests and
    automatically renew the access token if it is about to expire or has
    already expired. Here's an overview of the changes:
    
    - A new getAccessToken function is added to fetch the updated access
    token from the server using a refresh token.
    - The authLink is modified to include logic for checking the expiration
    of the access token and triggering a token renewal if necessary. The
    renewed access token is stored in the Redux store using store.dispatch.
    - The authLink is then concatenated with the httpLink to create the
    final Apollo Client link.
    - The Apollo Client instance is created with the updated link and an
    InMemoryCache.
    It's important to note that these changes only affect the request flow
    of Apollo Client, and the request flow of RTK Query remains unchanged.
    
    This implementation ensures that the user's access token is
    automatically renewed before it expires, providing a seamless
    authentication experience.

--- client/src/graphql/client.ts
+++ client/src/graphql/client.ts
@@ -1,21 +1,45 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import jwt_decode from 'jwt-decode';
 
 import store from '../store';
+import { authActions } from '../store/slices/auth';
+import AccessTokenPayload from '../interfaces/AccessTokenPayload';
 
 const httpLink = createHttpLink({
     uri: '/api/graphql',
 });
 
-const authLink = setContext((_: unknown, { headers }) => {
+const getAccessToken = async () => {
+    const res = await fetch('/api/auth/refresh', {
+        method: 'GET',
+        credentials: 'include',
+    });
+    const data: { accessToken: string } = await res.json();
+
+    return data.accessToken;
+};
+
+const authLink = setContext(async (_: unknown, { headers }) => {
     // get the access token from the Redux store
     // it's not possible to use the "useSelector" hook in non-component functions
-    const token = store.getState().auth.accessToken;
+    let accessToken = store.getState().auth.accessToken;
+    const decoded: AccessTokenPayload | null = accessToken
+        ? jwt_decode(accessToken)
+        : null;
+
+    // if the token expires in 5 seconds or has already expired
+    if (decoded && decoded.exp - Date.now() / 1000 < 5) {
+        // the token is about to expire, so we need to make a request to renew this token
+        accessToken = await getAccessToken();
+        store.dispatch(authActions.updateAccessToken(accessToken));
+    }
+
     // return the headers to the context so httpLink can read them
     return {
         headers: {
             ...headers,
-            authorization: token ? `Bearer ${token}` : '',
+            authorization: accessToken ? `Bearer ${accessToken}` : '',
         },
     };
 });
