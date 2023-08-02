01492c067df69c1f285befabccf9d082f1c88e5d
Oleksii Potapchuk <lyoshachuk@gmail.com>
2023-08-02 11:10:48 +0300

    refactor(API): create a dedicated service for Amazon S3 interactions
    This commit introduces a separate service for handling interactions
    with Amazon S3. Previously, the S3 client was exclusively utilized in
    the file-upload.ts file. However, considering potential future
    requirements, such as verifying the existence of a specified image in
    the S3 bucket, I have extracted the S3 client functionality into a
    distinct service.
    
    This refactoring enhances code organization and modularity, allowing
    the S3 client to be easily reused across different parts of the
    application.

--- server/src/models/file-upload.ts
+++ server/src/models/file-upload.ts
@@ -1,13 +1,7 @@
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
 
-const s3Client = new S3Client({
-    region: process.env.S3_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-    },
-});
+import s3Client from '../services/s3.service';
 
 interface GeneratePresignedUrlArgs {
     bucketName: string;
