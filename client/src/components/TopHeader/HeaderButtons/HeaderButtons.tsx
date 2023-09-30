1d3da1798ac23f51a9978dbed372799240567d46
Oleksii Potapchuk <lyoshachuk@gmail.com>
2023-09-30 09:31:46 +0300

    style(React <HeaderButtons />): remove unnecessary comments

--- client/src/components/TopHeader/HeaderButtons/HeaderButtons.tsx
+++ client/src/components/TopHeader/HeaderButtons/HeaderButtons.tsx
@@ -1,6 +1,5 @@
 import { FC, Fragment } from 'react';
 
-import ButtonLink from '../../UI/ButtonLink/ButtonLink';
 import classes from './HeaderButtons.module.css';
 import MyCartButton from '../MyCartButton/MyCartButton';
 import LogoutButton from '../LogoutButton/LogoutButton';
@@ -18,19 +17,7 @@ const HeaderButtons: FC<{ isAuth: boolean }> = ({ isAuth }) => {
             ) : (
                 <Fragment>
                     <SigninButton />
-                    {/* <ButtonLink
-                        to="/auth/sign-in"
-                        className={classes['side-links__item']}
-                    >
-                        Sign In
-                    </ButtonLink> */}
                     <SignupButton />
-                    {/* <ButtonLink
-                        to="/auth/sign-up"
-                        className={classes['side-links__item']}
-                    >
-                        Sign Up
-                    </ButtonLink> */}
                 </Fragment>
             )}
         </nav>
