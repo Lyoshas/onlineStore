807cb24c5284a5904fb79aa60bf81fc8dee3e946
Oleksii Potapchuk <lyoshachuk@gmail.com>
2023-12-21 09:17:28 +0200

    fix(API getProductCategories controller): handle async errors properly with asyncHandler
    Previously, if an unexpected error occurred in the getProductCategories
    controller method while a user was making a request, an error would be
    thrown internally, but the user's request would just hang. This commit
    fixes this by wrapping the controller method with 'asyncHandler'. Now,
    if the promise is rejected, Express will catch this error and the user
    will get an error message "Something went wrong".

--- server/src/controllers/product-category.ts
+++ server/src/controllers/product-category.ts
@@ -1,7 +1,10 @@
 import { RequestHandler } from 'express';
+import asyncHandler from 'express-async-handler';
 
 import { getProductCategories as retrieveProductCategories } from '../models/product-category.js';
 
-export const getProductCategories: RequestHandler = async (req, res, next) => {
-    res.json({ categories: await retrieveProductCategories() });
-};
+export const getProductCategories: RequestHandler = asyncHandler(
+    async (req, res, next) => {
+        res.json({ categories: await retrieveProductCategories() });
+    }
+);
