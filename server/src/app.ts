57840639fceace8449d5d76ff6264882fdb4b304
Oleksii Potapchuk <lyoshachuk@gmail.com>
2024-01-18 10:07:49 +0200

    feat(API): add the 'npm run knex' script
    'npm run knex' allows to run this command:
    'node --loader ts-node/esm ./node_modules/.bin/knex'.
    
    It's too much work to simply copy-paste this huge command whenever
    there's a need to perform some 'knex' command, so this commit creates a
    new script in the 'package.json' file that allows to reference this
    huge command with just 'npm run knex'.
    
    For example, if you want to create a new migration file, just run:
    $ npm run knex -- migrate:make migration_name
    Or if you want to create a new seed file:
    $ npm run knex -- seed:make seed_name

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import path from 'path';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { GraphQLFormattedError } from 'graphql';
