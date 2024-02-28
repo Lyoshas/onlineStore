1fa72cd9b3cc00069fc6901900c109bef08f18eb
Oleksii Potapchuk <lyoshachuk@gmail.com>
2024-02-28 12:02:16 +0200

    feat(API 'camelCaseObject'): add support for nested objects
    In the previous commit, the 'CamelCaseProperties' TypeScript interface
    was enhanced to support nested objects as well.
    
    Following that change, this commit modifies the 'camelCaseObject'
    function to add support for nested objects when transforming the keys of
    the provided object to the camelCase format.
    
    Example:
    camelCaseObject({
        first_key: 'val1',
        second_key: { third_key: 'a' }
    }) -> { firstKey: 'val1', secondKey: { thirdKey: 'a' } }

--- server/src/util/camelCaseObject.ts
+++ server/src/util/camelCaseObject.ts
@@ -1,19 +1,36 @@
+import AnyObject from '../interfaces/AnyObject.js';
 import CamelCaseProperties from '../interfaces/CamelCaseProperties.js';
 import snakeCaseToCamelCase from './snakeCaseToCamelCase.js';
 
-// Example:
-// camelCaseObject({ first_key: 'val1', second_key: 'val2' }) -> { firstKey: 'val1', secondKey: 'val2' }
-const camelCaseObject = <T extends object>(
-    inputObject: T
-): CamelCaseProperties<T> => {
-    const result = {} as CamelCaseProperties<T>;
+// this function takes an object and transforms all keys into the "camelCase"
+// format (nested objects are supported as well).
+// This function accepts only objects, but the implementation of this function
+// uses recursion, so "any" is used when performing recursive calls
 
-    for (let [key, value] of Object.entries(inputObject)) {
-        result[snakeCaseToCamelCase(key) as keyof CamelCaseProperties<T>] =
-            value;
+// Example:
+// camelCaseObject({ first_key: 'val1', second_key: { third_key: 'a' } }) -> { firstKey: 'val1', secondKey: { thirdKey: 'a' } }
+const camelCaseObject = <T extends object>(
+    input: T
+): CamelCaseProperties<T> => {
+    // if "input" is anything but an object or an array, return "input" immediately
+    if (
+        (typeof input !== 'object' && !Array.isArray(input)) ||
+        input === null
+    ) {
+        return input;
     }
 
-    return result;
+    // if we make it here, "input" is either an array or an object
+    if (Array.isArray(input)) {
+        return input.map((nestedValue) => camelCaseObject(nestedValue)) as any;
+    } else {
+        // "input" is an object
+        const result: AnyObject = {};
+        for (let [key, value] of Object.entries(input)) {
+            result[snakeCaseToCamelCase(key)] = camelCaseObject(value);
+        }
+        return result as any;
+    }
 };
 
 export default camelCaseObject;
