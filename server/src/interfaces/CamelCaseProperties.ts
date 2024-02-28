962cd4c266454345e55557880bffd722a1755325
Oleksii Potapchuk <lyoshachuk@gmail.com>
2024-02-28 11:11:33 +0200

    feat(API 'CamelCaseProperties' TS interface): add support for nested objects
    This enhancement extends the functionality of the 'CamelCaseProperties'
    TypeScript interface to handle nested objects. Previously, it only
    converted top-level keys to camelCase format. Now, it can handle nested
    structures as well.
    
    Example of using this interface:
    
    type OrderInfo = {
        user_id: number;
        delivery_warehouse_description: string;
        user_profile: { first_name: string, last_name: string };
        order_products: { title: string; quantity_to_order: number }[];
    };
    
    "CamelCaseProperties<OrderInfo>" will produce this type:
    {
        userId: number;
        deliveryWarehouseDescription: string;
        userProfile: { firstName: string; lastName: string };
        orderProducts: { title: string; quantityToOrder: number }[]
    }

--- server/src/interfaces/CamelCaseProperties.ts
+++ server/src/interfaces/CamelCaseProperties.ts
@@ -1,7 +1,32 @@
 import CamelCase from './CamelCase';
 
+// traverses object keys and maps them into the camelCase format
+// it can work with nested objects/arrays as well
+// example:
+/*
+    type OrderInfo = {
+        user_id: number;
+        delivery_warehouse_description: string;
+        user_profile: { first_name: string, last_name: string };
+        order_products: { title: string; quantity_to_order: number }[];
+    };
+
+    "CamelCaseProperties<OrderInfo>" will produce this type:
+    {
+        userId: number;
+        deliveryWarehouseDescription: string;
+        userProfile: { firstName: string; lastName: string };
+        orderProducts: { title: string; quantityToOrder: number }[]
+    }
+*/
 type CamelCaseProperties<T> = {
-    [K in keyof T as CamelCase<string & K>]: T[K];
+    [key in keyof T as CamelCase<string & key>]: T[key] extends Array<
+        infer NestedType
+    >
+        ? CamelCaseProperties<NestedType>[]
+        : T[key] extends object
+        ? CamelCaseProperties<T[key]>
+        : T[key];
 };
 
 export default CamelCaseProperties;
