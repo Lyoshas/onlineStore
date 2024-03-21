60992c03f057a25f784dddc5581c45651578528e
Oleksii Potapchuk <lyoshachuk@gmail.com>
2024-03-21 20:02:07 +0200

    feat(API): add a seed file for order recipients
    This commit creates a new file called '13_order_recipients.ts', which is
    located in the 'db-seeds' folder. This file is used to prepopulate the
    'order_recipients' DB table for testing purposes.

---
+++ server/db-seeds/13_order_recipients.ts
@@ -0,0 +1,46 @@
+import { Knex } from 'knex';
+import snakeCaseObject from '../src/util/snakeCaseObject.js';
+
+interface OrderRecipient {
+    associatedUserId: number;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+}
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex('order_recipients').del();
+
+    const getAdminUserIdSubquery = knex('users')
+        .select('id')
+        .where({ email: process.env.DB_SEEDING_ADMIN_EMAIL });
+
+    const orderRecipients: Omit<OrderRecipient, 'associatedUserId'>[] = [
+        {
+            firstName: process.env.DB_SEEDING_ADMIN_FIRST_NAME!,
+            lastName: process.env.DB_SEEDING_ADMIN_LAST_NAME!,
+            phoneNumber: process.env.DB_SEEDING_ADMIN_PHONE_NUMBER!,
+        },
+        {
+            firstName: 'Сергій',
+            lastName: 'Ткаченко',
+            phoneNumber: '+380-12-345-67-89',
+        },
+        {
+            firstName: 'Єлизавета',
+            lastName: 'Прокопенко',
+            phoneNumber: '+380-23-456-78-90',
+        },
+    ];
+
+    // Inserts seed entries
+    await knex('order_recipients').insert(
+        orderRecipients.map((recipient) =>
+            snakeCaseObject({
+                ...recipient,
+                associatedUserId: getAdminUserIdSubquery,
+            })
+        )
+    );
+}
