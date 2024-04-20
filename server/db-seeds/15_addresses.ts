b6ca7dd1c289dda5562cb867022ec3dd1cbb7d86
Oleksii Potapchuk <lyoshachuk@gmail.com>
2024-04-20 10:53:34 +0300

    feat(API seeds): add a seed file for the 'addresses' table
    Seed files are used to prepopulate database tables with sample data for
    testing purposes.
    
    This commit adds a file called '15_addresses.ts' that prepopulates the
    'addresses' table. These addresses will be used in the 'service_centers'
    table at some point in the future.

---
+++ server/db-seeds/15_addresses.ts
@@ -0,0 +1,50 @@
+import { Knex } from 'knex';
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex('addresses').del();
+
+    const addresses = [
+        {
+            id: 1,
+            street_name: 'Хрещатик',
+            building_number: '25А',
+            postal_code: '01001',
+        },
+        {
+            id: 2,
+            street_name: 'Дегтярівська',
+            building_number: '32',
+            postal_code: '02002',
+        },
+        {
+            id: 3,
+            street_name: 'Героїв Незалежності',
+            building_number: '16',
+            postal_code: '03003',
+        },
+        {
+            id: 4,
+            street_name: 'Львівська',
+            building_number: '7',
+            postal_code: '04004',
+        },
+        {
+            id: 5,
+            street_name: 'Саксаганського',
+            building_number: '1/36',
+            postal_code: '05005',
+        },
+    ];
+
+    // Inserts seed entries
+    await knex('addresses').insert(
+        addresses.map((address) => ({
+            ...address,
+            city_id: knex('cities')
+                .select('id')
+                .orderByRaw('RANDOM()')
+                .limit(1),
+        }))
+    );
+}
