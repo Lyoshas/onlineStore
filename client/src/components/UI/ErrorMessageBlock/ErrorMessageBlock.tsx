0770bd2d2aca52fd012eb4633e8338f3ee53f3f7
Oleksii Potapchuk <lyoshachuk@gmail.com>
2024-04-27 10:36:07 +0300

    feat(React <ErrorMessageBlock />): add 'whiteBackground' and 'buttonLinks' props
    The <ErrorMessageBlock /> component previously only supported the
    'message' prop, limiting its versatility. To enhance code reusability,
    two new props have been introduced for this component:
    
    - 'whiteBackground': A boolean indicating whether the error message
    should have a white background. Defaults to true.
    - 'buttonLinks': Allows specifying button links to display beneath the
    error message. This enables various actions, such as navigating to
    different pages.
    
    These additions aim to prevent code duplication, particularly in pages
    displaying orders and warranty requests.
    
    All existing code that utilized these components still function the same
    way as before, no breaking changes were introduced.

--- client/src/components/UI/ErrorMessageBlock/ErrorMessageBlock.tsx
+++ client/src/components/UI/ErrorMessageBlock/ErrorMessageBlock.tsx
@@ -1,16 +1,27 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 
 import CenterBlock from '../CenterBlock/CenterBlock';
 import ErrorIcon from '../Icons/ErrorIcon';
 import classes from './ErrorMessageBlock.module.css';
 import ButtonLink from '../ButtonLink/ButtonLink';
+import classNames from 'classnames';
 
-const ErrorMessageBlock: FC<{ message: string }> = ({ message }) => {
+const ErrorMessageBlock: FC<{
+    message: string;
+    buttonLinks?: React.ReactNode;
+    whiteBackground?: boolean;
+}> = ({ message, buttonLinks, whiteBackground = true }) => {
     return (
-        <CenterBlock className={classes['error-message-block']}>
+        <CenterBlock
+            className={classNames(
+                classes['error-message-block'],
+                classes[whiteBackground ? 'black-font' : 'white-font']
+            )}
+            whiteBackground={whiteBackground}
+        >
             <ErrorIcon className="icon" />
             <p>{message}</p>
-            <ButtonLink to="/">Home</ButtonLink>
+            {buttonLinks ? buttonLinks : <ButtonLink to="/">Home</ButtonLink>}
         </CenterBlock>
     );
 };
