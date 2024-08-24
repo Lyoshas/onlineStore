8402756293cf795d7a6e8b23838800d783014fe0
Oleksii Potapchuk <lyoshachuk@gmail.com>
2024-08-24 16:33:45 +0300

    fix(NestJS API): handle 404 routes correctly
    Previously, if a user tried to send a request to unregistered routes,
    the NestJS application responsed with the 500 error. This commit fixes
    this bug. Now the app will always return the 404 status code with the
    following response body:
    {
    	"errors": [
    		{
    			"message": "Endpoint not found"
    		}
    	]
    }

--- server-nest/src/common/filters/default-exception.filter.ts
+++ server-nest/src/common/filters/default-exception.filter.ts
@@ -3,6 +3,7 @@ import {
     Catch,
     ExceptionFilter,
     HttpStatus,
+    NotFoundException,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { ApplicationError } from '../interfaces/application-error.interface';
@@ -14,22 +15,19 @@ export class DefaultExceptionFilter implements ExceptionFilter {
     catch(exception: Error, host: ArgumentsHost) {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
-        const {
-            status,
-            errors,
-        }: { status: number; errors: ApplicationError[] } =
-            exception instanceof CustomException
-                ? {
-                      status: exception.getStatus(),
-                      errors: exception.errors,
-                  }
-                : {
-                      status: HttpStatus.INTERNAL_SERVER_ERROR,
-                      errors: [{ message: 'Something went wrong' }],
-                  };
 
-        if (!(exception instanceof CustomException)) {
+        let status: number, errors: ApplicationError[];
+
+        if (exception instanceof NotFoundException) {
+            status = 404;
+            errors = [{ message: 'Endpoint not found' }];
+        } else if (exception instanceof CustomException) {
+            status = exception.getStatus();
+            errors = exception.errors;
+        } else {
             console.error(exception);
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            errors = [{ message: 'Something went wrong' }];
         }
 
         response.status(status).json({ errors });
