f09d377c1635a46f952b20bef678be8bc21f5297
Oleksii Potapchuk <lyoshachuk@gmail.com>
2024-09-09 15:12:41 +0300

    refactor(NestJS DB seeding): make product category seeding type-safe
    Previously, the ProductCategorySeeder class inserted product categories
    into the database directly, which is a viable approach, but it's not
    great in the long run. It would be better to define a type that stores
    all the possible product categories, and then insert these categories
    according to this type. This would make the code more maintainable.
    
    Changes:
    - Created a separate ProductCategory type that stores all the possible
    product categories. This type can be reused across multiple seeders.
    Warning: this type is NOT meant to be used in the main codebase. It
    should only be used for DB seeding.
    - Created a private static property inside the ProductCategorySeeder
    class that stores different product categories as an array. Any category
    in this array must adhere to the ProductCategory type that was described
    above.

--- server-nest/src/seeds/seed-files/01-product-categories.ts
+++ server-nest/src/seeds/seed-files/01-product-categories.ts
@@ -1,71 +1,82 @@
 import { EntityManager } from 'typeorm';
 import { ProductCategory } from '../../products/entities/product-category.entity';
 import { Seedable } from '../interfaces/seedable.interface';
+import { ProductCategory as PossibleProductCategory } from '../types/product-category.type';
 
 export class ProductCategorySeeder implements Seedable {
+    // making this static so that this array is only generated once,
+    // not on every class instantiation
+    private static productCategories: {
+        category: PossibleProductCategory;
+        previewUrl: string;
+    }[] = [
+        {
+            category: 'Ігрові консолі',
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/consoles.png',
+        },
+        {
+            category: 'Ноутбуки',
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/laptops.png',
+        },
+        {
+            category: "Персональні комп'ютери",
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/personal-computers.png',
+        },
+        {
+            category: 'Планшети',
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/tablets.png',
+        },
+        {
+            category: 'Смартфони',
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/smartphones.png',
+        },
+        {
+            category: 'HDD',
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/hdd.png',
+        },
+        {
+            category: 'SSD',
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/ssd.png',
+        },
+        {
+            category: 'Процесори',
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/processors.png',
+        },
+        {
+            category: 'Відеокарти',
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/gpus.png',
+        },
+        {
+            category: "Оперативна пам'ять",
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/ram.png',
+        },
+        {
+            category: 'Монітори',
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/monitors.png',
+        },
+        {
+            category: 'Дрони',
+            previewUrl:
+                'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/drones.png',
+        },
+    ];
+
     async seed(manager: EntityManager): Promise<void> {
         await manager.clear(ProductCategory);
-        await manager.insert(ProductCategory, [
-            {
-                category: 'Ігрові консолі',
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/consoles.png',
-            },
-            {
-                category: 'Ноутбуки',
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/laptops.png',
-            },
-            {
-                category: "Персональні комп'ютери",
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/personal-computers.png',
-            },
-            {
-                category: 'Планшети',
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/tablets.png',
-            },
-            {
-                category: 'Смартфони',
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/smartphones.png',
-            },
-            {
-                category: 'HDD',
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/hdd.png',
-            },
-            {
-                category: 'SSD',
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/ssd.png',
-            },
-            {
-                category: 'Процесори',
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/processors.png',
-            },
-            {
-                category: 'Відеокарти',
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/gpus.png',
-            },
-            {
-                category: "Оперативна пам'ять",
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/ram.png',
-            },
-            {
-                category: 'Монітори',
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/monitors.png',
-            },
-            {
-                category: 'Дрони',
-                previewUrl:
-                    'https://onlinestore-react-assets.s3.amazonaws.com/product-categories/drones.png',
-            },
-        ]);
+        await manager.insert(
+            ProductCategory,
+            ProductCategorySeeder.productCategories
+        );
     }
 }
---
+++ server-nest/src/seeds/types/product-category.type.ts
@@ -0,0 +1,15 @@
+// don't use this type in the main codebase
+// it's only meant to be used for DB seeding
+export type ProductCategory =
+    | 'Ігрові консолі'
+    | 'Ноутбуки'
+    | "Персональні комп'ютери"
+    | 'Планшети'
+    | 'Смартфони'
+    | 'HDD'
+    | 'SSD'
+    | 'Процесори'
+    | 'Відеокарти'
+    | "Оперативна пам'ять"
+    | 'Монітори'
+    | 'Дрони';
