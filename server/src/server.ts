37b9c710a3f17adf172f9f292445d386049fe31e
Oleksii Potapchuk <lyoshachuk@gmail.com>
2024-09-30 21:10:01 +0300

    fix(Express): add cleanup logic on graceful termination
    This commit adds some cleanup logic whenever a user or another process
    tries to terminate the Node.js server. This is done by handling the
    SIGINT and SIGTERM events. This prevents potential memory leaks and
    allows the server to shut down faster with Docker Compose (previously,
    the Express server just "hanged" for ~10 seconds before Docker Compose
    sent the SIGKILL signal, which effectively kills the process without
    any ability to do some cleanup logic).

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,20 @@
 import app from './app.js';
+import osearchClient from './services/opensearch.service.js';
+import redisClient from './services/redis.service.js';
+import dbPool from './services/postgres.service.js';
 
-app.listen(3000, () => console.log('listening on port 3000'));
+const server = app.listen(3000, () => console.log('listening on port 3000'));
+
+const cleanup = () => {
+	server.close(async (err) => {
+        if (err) console.error(err);
+		await osearchClient.close();
+		await dbPool.end();
+		await redisClient.quit();
+	});
+};
+
+// termination signal that is usually invoked by other processes
+process.on('SIGTERM', () => cleanup);
+// termination signal that is usually invoked by the user
+process.on('SIGINT', () => cleanup);
